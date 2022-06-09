962c2e4db1acd44a34d7ef50f36c3385127604b1
knyazevilya <kknyaz033@gmail.com>
2022-06-09 20:49:22 +0300

    Initial Commit

---
+++ HeatControl/HeatControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeatControlApp.swift
+//  HeatControl
+//
+//  Created by Illia Kniaziev on 09.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct HeatControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
